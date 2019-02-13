b9d1fcf3baa44bf9abc4da7e2ed08ef76c568075
bwahler <bmwahler@gmail.com>
2019-02-12 21:56:35 -0500

    Initial commit

---
+++ CoffeeShop/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using CoffeeShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult Welcome(User u)
+        {
+            ViewBag.FirstName = u.FirstName;
+            ViewBag.LastName = u.LastName;
+            ViewBag.Password = u.Password;
+            ViewBag.Email = u.Email;
+            ViewBag.PhoneNumber = u.PhoneNumber;
+            return View();
+        }
+    }
+}
